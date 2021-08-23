5dcf3000344abfdc421da461dca7e16bcef2b4b9
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-08-23 01:35:19 +0100

    Update ProductController.java

--- src/main/java/com/javatodev/api/controller/ProductController.java
+++ src/main/java/com/javatodev/api/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/product")
+@RequestMapping(value = "/")
 @RequiredArgsConstructor
 public class ProductController {
 
