ca5ceb411a44e70087bc0b787dd53d5d9a271b05
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-08-23 01:11:54 +0100

    removed constraints

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ CREATE TABLE Product(
     id INTEGER NOT NULL DEFAULT ,
     image_url varchar(255) NULL,
     name varchar(255) NULL,
-    CONSTRAINT product_pkey PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 INSERT INTO Product(image_url, name) VALUES ('https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/23420/image/1462221519/600.jpg', 'CREMEFRAICHE');
 INSERT INTO Product(image_url, name) VALUES ('https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/213942/image/1560262700/600.jpg', 'PEPSI MAX');
