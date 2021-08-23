9077c43d475c1e365da58347c7c3699a16258957
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-08-23 01:26:55 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Product(
+/*CREATE TABLE Product(
     id INTEGER NOT NULL DEFAULT ,
     image_url varchar(255) NULL,
     name varchar(255) NULL,
     PRIMARY KEY (id)
-);
+);*/
 INSERT INTO Product(image_url, name) VALUES ('https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/23420/image/1462221519/600.jpg', 'CREMEFRAICHE');
 INSERT INTO Product(image_url, name) VALUES ('https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/213942/image/1560262700/600.jpg', 'PEPSI MAX');
 INSERT INTO Product(image_url, name) VALUES ('https://cphapp.rema1000.dk/api/v1/catalog/store/1/item/201003/image/1553075410/600.jpg', 'CARLSBERG');
